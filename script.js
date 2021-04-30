45c4ef2296b5c00f39f926a7df0fb349d08b8f44
samcoombs <76431238+samcoombs@users.noreply.github.com>
2021-04-30 10:13:37 +0100

    Updates
    New Project Added: Onward Travel.
    Old Projects Removed.
    _work.scss Code Reduced.
    Fixed: Could still scroll even when the mobile nav was open.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const burgerMenu = document.querySelector(".burger");
 const nav = document.querySelector(".nav");
 const navList = document.querySelector(".nav__list");
 const navLink = document.querySelector(".nav__link");
+const body = document.querySelector("body");
 
 let burgerOpen = false;
 
@@ -10,10 +11,12 @@ const menuToggle = function () {
   if (!burgerOpen) {
     burgerMenu.classList.add("open");
     nav.classList.add("open");
+    body.style.overflow = "hidden";
     burgerOpen = true;
   } else {
     burgerMenu.classList.remove("open");
     nav.classList.remove("open");
+    body.style.overflow = "scroll";
     burgerOpen = false;
   }
 };
